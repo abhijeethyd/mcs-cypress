a1f28cf80c72ebc03fadbfbde9f7289b8a4a588d
abhijeethyd <abhijeet.hyd@gmail.com>
2021-06-16 01:28:58 +1200

    add
    wq!
    w! with '#' will be ignored, and an empty message aborts the commit.

--- cypress/integration/mcs/login.spec.js
+++ cypress/integration/mcs/login.spec.js
@@ -11,12 +11,12 @@ context("Actions", () => {
     it("Create a WebApp without github and F1 Account", () => {
       cy.login('SimpleMenuUser','123zxc')
       cy.get("h5").contains('Create a WebApp').click();
-      cy.get("input[ng-model='vm.UMCR.Name']").type("App"+uniqueSeed);
+      cy.get("input[ng-model='vm.UMCR.Name']").click().type("App"+uniqueSeed);
       cy.get("button[data-id='SelectAppserviceplanCombobox']").click();
       cy.get("a").contains('F1').click();
       cy.get("button").contains('Build').click();
+      cy.wait(500)
       cy.get("button[class='confirm']").click();
-      cy.wait(20000);
     });
 
   /*  it("Create a WebApp with connected github and F1 Account", () => {

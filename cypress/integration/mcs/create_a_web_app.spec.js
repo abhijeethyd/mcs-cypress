6196362eab19a1cd34cfbafcb7915dd626e77ba4
Abhijeet <abhijeet.hyd@gmail.com>
2021-08-28 02:15:13 +1200

    Refactored Create a Webapp Test

--- cypress/integration/mcs/create_a_web_app.spec.js
+++ cypress/integration/mcs/create_a_web_app.spec.js
@@ -1,27 +1,23 @@
 /// <reference types="cypress" />
 import '../../support/commands.js'
 
-
-
-context("Actions", () => {
-    
-    it("Create a WebApp without github and Free Account", () => {
-      const uniqueSeed = Date.now().toString();
-      cy.login('SimpleMenuUser','123zxc')
+context("Create a WebApp", () => {
+  
+    it("Without github and Free Account plan 'Free - 1 - 1.75GB - 1GB - Free - Free'", () => {
+      const appName = "App" + Date.now().toString();
+      cy.login('SimpleMenuUser','123zxc');
       cy.get("h5").contains('Create a WebApp').click();
-      cy.get("input[ng-model='vm.UMCR.Name']",{ timeout: 10000 }).should('be.visible').click().type("App"+uniqueSeed);
+      cy.get("input[ng-model='vm.UMCR.Name']",{ timeout: 20000 }).should('be.visible').click().type(appName);
       cy.get("button[data-id='SelectAppserviceplanCombobox']",{ timeout: 10000 }).should('be.visible').click();
       cy.get("a").contains('Free - 1 - 1.75GB - 1GB - Free - Free').click();
-      cy.get("button").contains('Build').click();
-    });
-
-  /*  it("Create a WebApp with connected github and F1 Account", () => {
-      cy.skip();
-      cy.login('SimpleMenuUser','123zxc')
-      cy.get("h5").contains('Create a WebApp').click();
-      cy.get("input[ng-model='vm.UMCR.Name']").click();
-      cy.get("input[ng-model='vm.UMCR.Name']").type("Abhi-Cypress");
-      cy.get("button[data-id='SelectAppserviceplanCombobox']").click();
-      cy.get("a").contains('F1').click();
-    });*/
+      cy.get("button").contains('Build').click();      
+      cy.get('#BuildWebApp > div > table > tbody > tr:nth-child(1) > td > div > label').should('contain.text', 'Build Resource Group')
+      cy.get('#BuildWebApp > div > table > tbody > tr:nth-child(1) > td > div > i',{ timeout: 20000 }).should('be.visible').should('contain.text', 'Success');
+      cy.get('#BuildWebApp > div > table > tbody > tr:nth-child(2) > td > div > label').should('contain.text', 'Build Service Plan')
+      cy.get('#BuildWebApp > div > table > tbody > tr:nth-child(2) > td > div > i',{ timeout: 20000 }).should('be.visible').should('contain.text', 'Success');
+      cy.get('#BuildWebApp > div > table > tbody > tr:nth-child(3) > td > div > label').should('contain.text', 'Build Web App')
+      cy.get('#BuildWebApp > div > table > tbody > tr:nth-child(3) > td > div > i',{ timeout: 80000 }).should('be.visible').should('contain.text', 'Success');
+      cy.get("button").contains('Back').click(); 
+      cy.get("a>h5>span[class='ng-binding']").contains(appName).click();
+    })
 });
